d6435b0ebf9e4ad604503befafd5e964998f2a10
markozeman <mz4730@student.uni-lj.si>
2017-04-14 14:19:34 +0200

    API call working without commodities

--- code/UNComtrade.py
+++ code/UNComtrade.py
@@ -33,18 +33,40 @@ class UNComtrade:
 
     # partners, time_period, trade_flows,
     def call_api (self, reporters, partners, time_period, trade_flows, type='C', freq='A', classification='HS', commodities='AG2', max_values=500, head='H', format='json'):
-        r = check_form(reporters)
-        p = check_form(partners)
-        ps = check_form(time_period)
-        rg = check_form(trade_flows)
+        r = check_form(reporters, 'r')
+        p = check_form(partners, 'p')
+        ps = check_form(time_period, 'ps')
+        rg = check_form(trade_flows, 'rg')
 
-        if (r is not None and p is not None and ps is not None and rg is not None):
+        if (r is not None and p is not None and ps is not None and rg is not None):     # če so vsi veljavni
             # print(r,p,ps,rg)
 
-            # TODO - največ en all, razen rg je vseeno lahko
+            # največ en od prvih treh je lahko all
+            if ((r == 'all' and p == 'all') or (r == 'all' and ps == 'all') or (p == 'all' and ps == 'all')):
+                print("Only one of reporters, partners and time_period can be set to all")
+            else:
+                base_URL = 'http://comtrade.un.org/api/get?'
 
-            pass
+                # TODO
+                # commodities?
 
+
+                URL_parameters = 'type='+type+'&freq='+freq+'&r='+r+'&p='+p+'&ps='+ps+'&px='+classification+'&rg='\
+                                 +rg+'&cc='+commodities+'&fmt='+format+'&head='+head+'&max='+str(max_values)
+
+                URL = base_URL + URL_parameters
+
+                print(URL)
+
+                req = requests.get(URL)
+                req_json = req.json()
+
+                print('Message:', req_json['validation']['message'])
+                print('Total values:', req_json['validation']['count']['value'])
+                print('API call took ' + str(req_json['validation']['datasetTimer']['durationSeconds']) + ' seconds')
+
+                for record in req_json['dataset']:
+                    print(record)
 
 
 
@@ -65,16 +87,35 @@ def json2object(filename):
         return data_object
 
 
-def check_form(parameter):
+def check_form(parameter, p):
+    need_id = True
+
+    if (p == 'r'):
+        dict = json2object('data/reporters.json')
+    elif (p == 'p'):
+        dict = json2object('data/partners.json')
+    elif (p == 'rg'):
+        dict = json2object('data/trade_flows.json')
+    else:
+        need_id = False
+
     s = ''
-    if (isinstance(parameter, str) or isinstance(parameter, int)):
+    if (isinstance(parameter, str)):
+        if (need_id and parameter != 'all' and parameter in dict):
+            s = dict[str(parameter)]
+        else:
+            s = str(parameter)
+    elif (isinstance(parameter, int)):
         s = str(parameter)
     elif (isinstance(parameter, list)):
         if (len(parameter) > 5):
             print("You can choose max 5 values.")
             return None
         for i in range(len(parameter)):
-            s += str(parameter[i])
+            if (need_id and parameter[i] != 'all' and parameter[i] in dict):
+                s += str(dict[str(parameter[i])])
+            else:
+                s += str(parameter[i])
 
             if (str(parameter[i]).lower()[:3] == 'all'):
                 s = 'all'
@@ -85,31 +126,29 @@ def check_form(parameter):
             if (i < len(parameter) - 1):
                 s += '%2C'
     else:
-        print("Parameter must be a string or a list.")
+        print("Parameter must be a string, integer or a list.")
         return None
 
     return s
 
 
 
-
 def print_all():
     unc = UNComtrade()
-    print(unc.years())
-    print(unc.reporters())
-    print(unc.partners())
-    print(unc.commodities_HS())
-    print(unc.commodities_ST())
-    print(unc.commodities_BEC())
-    print(unc.services())
-    print(unc.trade_flows())
-    unc.call_api(['Albania', 'all'], 'Croatia', ['all', 2010, 2012, 2013], ['Alll cdsubufs', 'import'])
+    # print(unc.years())
+    # print(unc.reporters())
+    # print(unc.partners())
+    # print(unc.commodities_HS())
+    # print(unc.commodities_ST())
+    # print(unc.commodities_BEC())
+    # print(unc.services())
+    # print(unc.trade_flows())
+    unc.call_api(['Albania', 'all'], 'Slovenia', [2010,2012], ['Alll cdsubufs', 'import'], commodities='AG4')
 
 
 
 print_all()
 
-# req = requests.get('https://comtrade.un.org/api/get?ps=all&p=0&px=HS&r=4&rg=all&cc=AG4&max=1000&head=M')
-# print(req.json())
+
 
 
