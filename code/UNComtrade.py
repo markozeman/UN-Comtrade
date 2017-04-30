dd3416f531daa4054f0130ed0441a7362ec87a48
markozeman <mz4730@student.uni-lj.si>
2017-04-30 22:38:33 +0200

    All unit tests + validation

--- code/UNComtrade.py
+++ code/UNComtrade.py
@@ -33,45 +33,64 @@ class UNComtrade:
     # partners, time_period, trade_flows,
     def call_api (self, reporters, partners, time_period, trade_flows, type='C', freq='A', classification='HS',
                   commodities='AG2 - All 2-digit HS commodities', max_values=10, head='H', format='json'):
+
+        # check if optional parameters are ok
+        if (not check_optional_parameters(type, freq, classification, max_values, head, format)):
+            return 1
+
+        if (type == 'S'):
+            classification = 'EB02'
+            commodities = check_if_commodities_default(commodities, 'TOTAL - Total EBOPS 2002 Services')
+
+        if (classification == 'ST'):
+            commodities = check_if_commodities_default(commodities, 'AG2 - All 2-digit SITC commodities')
+        elif (classification == 'BEC'):
+            commodities = check_if_commodities_default(commodities, 'AG2 - All 2-digit BEC commodities')
+
         r = check_form(reporters, 'r')
         p = check_form(partners, 'p')
         ps = check_form(time_period, 'ps')
         rg = check_form(trade_flows, 'rg')
-        cc = check_form(commodities, 'cc')
+        cc = check_form(commodities, 'cc', classified=classification, freq=freq)
 
         if (r is not None and p is not None and ps is not None and rg is not None and cc is not None):     # če so vsi veljavni
-            # print(r,p,ps,rg)
-
             # največ en od prvih treh je lahko all
             if ((r == 'all' and p == 'all') or (r == 'all' and ps == 'all') or (p == 'all' and ps == 'all')):
-                print("Only one of reporters, partners and time_period can be set to all")
+                print("Only one of reporters, partners and time_period can be set to all.")
+                return 2
             else:
                 base_URL = 'http://comtrade.un.org/api/get?'
-
-                # TODO
-                # commodities?
-
-
                 URL_parameters = 'type='+type+'&freq='+freq+'&r='+r+'&p='+p+'&ps='+ps+'&px='+classification+'&rg='\
                                  +rg+'&cc='+cc+'&fmt='+format+'&head='+head+'&max='+str(max_values)
-
                 URL = base_URL + URL_parameters
 
-                print(URL)
-
                 req = requests.get(URL)
-                req_json = req.json()
 
-                print('Message:', req_json['validation']['message'])
-                print('Total values:', req_json['validation']['count']['value'])
-                print('Returned values:', req_json['validation']['count']['value'] if max_values > req_json['validation']['count']['value'] else max_values)
-                print('API call took ' + "{0:.2f}".format(req_json['validation']['datasetTimer']['durationSeconds']) + ' seconds')
+                if (format == 'json'):
+                    print(req)
+                    req_json = req.json()
 
-                for record in req_json['dataset']:
-                    print(record)
+                    print(URL)
+                    print('Message:', req_json['validation']['message'])
+                    print('Total values:', req_json['validation']['count']['value'])
+                    print('Returned values:',
+                          req_json['validation']['count']['value'] if max_values > req_json['validation']['count'][
+                              'value'] else max_values)
+                    print('API call took ' + "{0:.2f}".format(
+                        req_json['validation']['datasetTimer']['durationSeconds']) + ' seconds')
 
+                    for record in req_json['dataset']:
+                        print(record)
+
+                    return req_json['dataset']
+
+                elif (format == 'csv'):
+                    req_csv = req.content
+                    print(req_csv)
+                    return req_csv
         else:
             print('\nOne of the parameters is not valid.\n')
+            return 3
 
 
 
@@ -90,17 +109,72 @@ def json2object(filename):
         return data_object
 
 
-def check_form(parameter, p):
+def check_if_commodities_default(comm, new):
+    if (comm == 'AG2 - All 2-digit HS commodities'):
+        return new
+    return comm
+
+
+def check_optional_parameters(t, fr, c, mv, h, f):
+    ok = True
+    if (t != 'C' and t != 'S'):
+        print('Type parameter is invalid.')
+        ok = False
+    if (fr != 'A' and fr != 'M'):
+        print('Frequency parameter is invalid.')
+        ok = False
+    if (c != 'HS' and c != 'ST' and c != 'BEC' and c != 'EB02'):
+        print('Classification parameter is invalid.')
+        ok = False
+    if (isinstance(mv, str) or (isinstance(mv, int) and mv < 0 or mv > 100000)):
+        print('Max values parameter is invalid.')
+        ok = False
+    if (h != 'H' and h != 'M'):
+        print('Head parameter is invalid.')
+        ok = False
+    if (f != 'json' and f != 'csv'):
+        print('Format parameter is invalid.')
+        ok = False
+    return ok
+
+
+def check_form(parameter, p, classified='', freq=''):
     need_id = True
 
     if (p == 'r'):
-        dict = json2object('data/reporters.json')
+        dict = json2object('../code/data/reporters.json')
     elif (p == 'p'):
-        dict = json2object('data/partners.json')
+        dict = json2object('../code/data/partners.json')
     elif (p == 'rg'):
-        dict = json2object('data/trade_flows.json')
+        dict = json2object('../code/data/trade_flows.json')
     elif (p == 'cc'):
-        dict = json2object('data/commodities_HS.json')
+        if (classified == 'HS'):
+            dict = json2object('../code/data/commodities_HS.json')
+        elif (classified == 'ST'):
+            dict = json2object('../code/data/commodities_ST.json')
+        elif (classified == 'BEC'):
+            dict = json2object('../code/data/commodities_BEC.json')
+        elif (classified == 'EB02'):
+            dict = json2object('../code/data/services.json')
+        else:
+            print('Wrong classification.')
+            return None
+    elif (p == 'ps'):
+        if (freq == 'A'):
+            if (isinstance(parameter, int) and (parameter < 1962 or parameter > 2050)):
+                print('Year is not correct')
+                return None
+            elif (isinstance(parameter, list)):
+                if (parameter[0] != 'all'):
+                    for y in parameter:
+                        if (y < 1962 or y > 2050):
+                            print('Year is not correct')
+                            return None
+            elif (isinstance(parameter, str)):
+                if (parameter != 'all'):
+                    print('Year is not correct')
+                    return None
+        need_id = False
     else:
         need_id = False
 
@@ -117,7 +191,10 @@ def check_form(parameter, p):
     elif (isinstance(parameter, int)):
         s = str(parameter)
     elif (isinstance(parameter, list)):
-        if (len(parameter) > 5):
+        if (len(parameter) > 20 and p == 'cc'):
+            print("You can choose max 20 commodities.")
+            return None
+        if (len(parameter) > 5 and p != 'cc'):
             print("You can choose max 5 values.")
             return None
         for i in range(len(parameter)):
@@ -151,7 +228,8 @@ def print_all():
     # print(unc.commodities_BEC())
     # print(unc.services())
     # print(unc.trade_flows())
-    unc.call_api('All', 'Slovenia', [2006, ], 'Export', commodities='TOTAL - Total of all HS commodities')
+    r = unc.call_api('Norway', ['Finland', 'Denmark'], [2014, 2015], 'All', max_values=1000)
+    # print(r)
 
 
 # print_all()
--- tests/unittests.py
+++ tests/unittests.py
@@ -49,9 +49,159 @@ class TestPossibleParameters(unittest.TestCase):
 
 
 class TestAPICalls(unittest.TestCase):
-    pass
+
+    def test_restrictions(self):
+        # max five reporters
+        r = unc.call_api(['Croatia', 'Italy', 'Austria', 'Hungary', 'Germany', 'France'], 'Slovenia', ['all'], 'Export')
+        self.assertEqual(r, 3)
+
+        # max five partners
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy', 'Austria', 'Hungary', 'Germany', 'France'], ['all'], 'Export')
+        self.assertEqual(r, 3)
+
+        # max five years
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy', 'Austria', 'Hungary'], [2007,2008,2009,2010,2011,2012,2013], 'Export')
+        self.assertEqual(r, 3)
+
+        # max 20 commodities
+        comm = ['01 - Live animals', '0101 - Live horses, asses, mules and hinnies', '010110 - Live horses/asses/mules/hinnies: pure-bred breeding animals',
+                '010111 - Horses, live pure-bred breeding', '010119 - Horses, live except pure-bred breeding', '010120 - Asses, mules and hinnies, live',
+                '010121 - Live animals // Live horses, asses, mules and hinnies. // - Horses : // -- Pure-bred breeding animals',
+                '010129 - Live animals // Live horses, asses, mules and hinnies. // - Horses : // -- Other',
+                '010130 - Live animals // Live horses, asses, mules and hinnies. // -Asses',
+                '010190 - Live horses/asses/mules/hinnies other than pure-bred breeding animals',
+                '0102 - Live bovine animals', '010210 - Bovine animals, live pure-bred breeding',
+                '010221 - Live animals // Live bovine animals. // - Cattle : // -- Pure-bred breeding animals',
+                '010229 - Live animals // Live bovine animals. // - Cattle : // -- Other',
+                '010231 - Live animals // Live bovine animals. // - Buffalo : // -- Pure-bred breeding animals',
+                '010239 - Live animals // Live bovine animals. // - Buffalo : // -- Other', '010290 - Bovine animals, live, except pure-bred breeding',
+                '0103 - Live swine', '010310 - Swine, live pure-bred breeding', '010391 - Swine, live except pure-bred breeding < 50 kg',
+                '010392 - Swine, live except pure-bred breeding > 50 kg', '0104 - Live sheep and goats', '010410 - Sheep, live']
+
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2007, 2008, 2009], 'Export', commodities=comm)
+        self.assertEqual(r, 3)
+
+        # max 1 'all' in reporters, partners and years
+        r = unc.call_api('all', ['Croatia', 'Italy'], 'all', 'Export')
+        self.assertEqual(r, 2)
+
+
+    def test_parameter_all(self):
+        # if parameter 'all' is chosen, others don't matter
+        r_1 = unc.call_api(['all'], ['Croatia', 'Italy'], 2015, 'Import')
+        r_2 = unc.call_api(['Slovenia', 'all'], ['Croatia', 'Italy'], 2015, 'Import')
+        self.assertEqual(len(r_1), len(r_2))
+
+        r_1 = unc.call_api('Slovenia', ['all', 'Italy'], 2015, 'Import')
+        r_2 = unc.call_api('Slovenia', ['all'], 2015, 'Import')
+        self.assertEqual(len(r_1), len(r_2))
+
+
+    def test_optional_parameters(self):
+        # annual vs monthly
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2014, 2015], 'Export', freq='A')
+        self.assertNotEqual(r, 1)
+        self.assertNotEqual(r, 2)
+        self.assertNotEqual(r, 3)
+
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2014, 2015], 'Export', freq='M')
+        self.assertEqual(len(r), 0)
+
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [201401, 201503], 'Export', freq='M')
+        self.assertNotEqual(len(r), 0)
+
+        # services
+        r = unc.call_api('All', 'Slovenia', [2015], 'Export', type='S')
+        self.assertEqual(len(r), 5)
+
+        # max values
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2015], 'Export', max_values=-5)
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2015], 'Export', max_values=150000)
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', max_values=100)
+        self.assertEqual(len(r), 100)
+
+        # classification
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', classification='WRONG')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', classification='HS')
+        self.assertNotEqual(len(r), 0)
+
+
+    def test_wrong_inputs(self):
+        # non-existing country
+        r = unc.call_api('Lovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export')
+        self.assertEqual(r, 3)
+
+        # non-existing year
+        r = unc.call_api('Lovenia', ['Croatia', 'Italy'], [2013, 2014, 2150], 'Export')
+        self.assertEqual(r, 3)
+
+        # non-existing trade_flows
+        r = unc.call_api('Lovenia', ['Croatia', 'Italy'], [2013, 2014, 2150], 'Import-Export')
+        self.assertEqual(r, 3)
+
+        # wrong input for optional parameters
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', type='L')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', freq='L')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', head='L')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', format='L')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', classification='L')
+        self.assertEqual(r, 1)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', commodities='L')
+        self.assertEqual(r, 3)
+        r = unc.call_api('Slovenia', ['Croatia', 'Italy'], [2013, 2014, 2015], 'Export', max_values='L')
+        self.assertEqual(r, 1)
+
+
+    def test_random_calls(self):
+        r = unc.call_api('Slovenia', ['Italy'], 2015, 'Import', max_values=1000)
+        self.assertEqual(len(r), 95)
+
+        r = unc.call_api('Slovenia', ['France'], [2010, 2015], 'Import', max_values=1000)
+        self.assertEqual(len(r), 187)
+
+        # r = unc.call_api('USA', ['France'], [2000, 2001], 'Import', max_values=1000)
+        # self.assertEqual(len(r), 194)
+
+        # r = unc.call_api('USA', ['France'], [2000, 2001], 'All', max_values=1000)
+        # self.assertEqual(len(r), 572)
+
+        # r = unc.call_api('Slovenia', ['Germany'], [2010], 'All', max_values=1000)
+        # self.assertEqual(len(r), 189)
+
+        # r = unc.call_api('Slovenia', ['Cuba'], [2010], 'Import', max_values=1000)
+        # self.assertEqual(len(r), 10)
+        # self.assertEqual(r[0]['TradeValue'], 67035)
+        # self.assertEqual(r[1]['TradeValue'], 54)
+
+        # r = unc.call_api('Slovenia', ['Denmark'], [2011], 'Import', max_values=1000)
+        # self.assertEqual(len(r), 82)
+        # self.assertEqual(r[0]['TradeValue'], 46934)
+        # self.assertEqual(r[1]['TradeValue'], 918870)
+
+        # r = unc.call_api('Slovenia', ['Sweden'], [2011], 'Import', max_values=1000)
+        # self.assertEqual(len(r), 75)
+        # self.assertEqual(r[0]['TradeValue'], 772133)
+        # self.assertEqual(r[1]['TradeValue'], 108040)
+
+        r = unc.call_api('Norway', ['Finland'], [2015], 'Import', max_values=1000)
+        self.assertEqual(len(r), 96)
+        self.assertEqual(r[0]['TradeValue'], 35226)
+        self.assertEqual(r[1]['TradeValue'], 1486708)
+
+        # r = unc.call_api('Norway', ['Finland', 'Denmark'], [2014, 2015], 'All', max_values=1000)
+        # self.assertEqual(len(r), 750)
+        # self.assertEqual(r[0]['TradeValue'], 10095648)
+        # self.assertEqual(r[1]['TradeValue'], 972794)
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
